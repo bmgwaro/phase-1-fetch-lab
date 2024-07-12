805dda887287f551ee5422c2db53cdd2f847d998
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-07-12 12:40:31 +0300

    fetch ab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
   .then(res=>res.json())
   .then(books=>renderBooks(books))
 }
@@ -9,11 +9,8 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerText = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-});
